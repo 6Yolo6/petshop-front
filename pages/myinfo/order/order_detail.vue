0dcf0f63ad36f2ad237d3c1e31cc24f1ab98b283
yolo <2031798793@qq.com>
2023-09-13 10:49:31 +0800

    订单流程,

---
+++ pages/myinfo/order/order_detail.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view class="head">
+			<u-navbar title="订单详情" :autoBack="true">
+			</u-navbar>
+		</view>
+		<view class="tabs">
+
+		</view>
+		<view class="content">
+			<view>
+				666
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		mounted() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
