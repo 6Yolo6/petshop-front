dc8e9353e3d295fb14c9981898a841a09f18c37d
hongxinchen <1549156292@qq.com>
2023-09-19 15:21:38 +0800

    完善搜索页面，添加用户搜索历史记录

--- api/modules/search.js
+++ api/modules/search.js
@@ -2,3 +2,12 @@ import { get, post, postJson } from '../axios.js'
 export function searchByKey(params) {
 	return get('/search/searchByKey', params)
 }
+export function getUserSearchHistory(params) {
+	return get('/search/getUserSearchHistory', params)
+}
+export function deleteUserSearchHistory(params) {
+	return post('/search/deleteUserSearchHistory', params)
+}
+export function clearUserSearchHistory(params) {
+	return post('/search/clearUserSearchHistory', params)
+}
--- pages/index/search.vue
+++ pages/index/search.vue
@@ -10,7 +10,7 @@
 				<uni-col :span="20">
 					<u-search :clearabled="true" shape="round" :value="inp_value" :placeholder="tip" @search="search"
 						bgColor="linear-gradient(to right, #70e1f5, #ffd194)" :focus="true"
-						:showAction="false"></u-search>
+						@focus="getUserSearchHistory()" :showAction="false"></u-search>
 				</uni-col>
 				<!-- 选择搜索类型 -->
 				<uni-col :span="4">
@@ -19,6 +19,31 @@
 				</uni-col>
 			</uni-row>
 		</view>
+		<!-- 搜索记录 -->
+		<view>
+			<u-popup :show="show" mode="left" @close="close" :round="10" :safeAreaInsetTop="true">
+				<view>
+					<u-list class="search-history" style="max-width: 35vh;">
+						<u-list-item v-for="(item, index) in history_list" :key="index">
+							<u-cell>
+								<view slot="title">
+									<u--text type="info" :text="item" @click="autoAssign(item)" :lines="2"></u--text>
+								</view>
+								<view slot="value">
+									<u-button type="error" shape="circle" plain size="mini" icon="close"
+										@click="deleteHistory(item)"></u-button>
+								</view>
+							</u-cell>
+						</u-list-item>
+					</u-list>
+				</view>
+				<view class="clear">
+					<u-button v-if="isShowClearHistory" type="primary" text="清空历史" shape="circle"
+						@click="showConform=true"></u-button>
+				</view>
+			</u-popup>
+		</view>
+		<!-- <u-toast ref="uToast"></u-toast> -->
 		<!-- 搜索结果 -->
 		<view class="list">
 			<view v-for="(item,index) in result_list" :key="index">
@@ -43,16 +68,27 @@
 			</view>
 		</view>
 		<u-toast ref="uToast"></u-toast>
+		<u-modal :show="showConform" :closeOnClickOverlay="true" :content="confirmContent" :showCancelButton="true"
+			@confirm='confirmYes()' @cancel="confirmNo" @close="clickOverlay"></u-modal>
 	</view>
 </template>
 
 <script>
 	import {
-		searchByKey
+		searchByKey,
+		getUserSearchHistory,
+		deleteUserSearchHistory,
+		clearUserSearchHistory
 	} from '@/api/modules/search.js'
 	export default {
 		data() {
 			return {
+				isShowClearHistory: true,
+				showConform: false,
+				confirmContent: "您确定要清空历史记录吗？",
+				history_list: [],
+				// 显示历史记录弹出层
+				show: false,
 				selected_value: 0,
 				inp_value: '',
 				tip: '请输入关键词',
@@ -73,7 +109,52 @@
 				result_list: [],
 			}
 		},
+		mounted() {
+			this.getUserSearchHistory()
+		},
 		methods: {
+			// 清空历史记录
+			confirmYes() {
+				this.clearHistory()
+				this.showConform = false
+			},
+			clickOverlay() {
+				this.showConform = false
+			},
+			confirmNo() {
+				this.showConform = false
+			},
+			// 清空用户历史记录
+			clearHistory() {
+				clearUserSearchHistory().then(res => {
+					console.log(res)
+					this.getUserSearchHistory()
+				})
+			},
+			// 删除单个历史记录
+			deleteHistory(key) {
+				deleteUserSearchHistory({ key: key }).then(res => {
+					console.log(res)
+					// this.$refs.uToast.show({
+					// 	type: 'success',
+					// 	title: '失败主题',
+					// 	message: "删除成功",
+					// 	iconUrl: 'https://cdn.uviewui.com/uview/demo/toast/success.png'
+					// })
+					this.getUserSearchHistory()
+				})
+			},
+			// 自动填充关键词
+			autoAssign(value) {
+				this.inp_value = value
+				this.search(value)
+				console.log(value)
+			},
+			// 关闭遮罩层
+			close() {
+				this.show = false
+				// console.log('close');
+			},
 			change(value) {
 				this.inp_value = ''
 				this.result_list = []
@@ -85,10 +166,24 @@
 					delta: 1, // 返回的层数，1表示返回上一个页面
 				});
 			},
-
+			//获取搜索记录
+			getUserSearchHistory() {
+				getUserSearchHistory().then(res => {
+					this.history_list = res.data.data
+					if (this.history_list.length == 0) {
+						this.isShowClearHistory = false
+						this.show = false
+					} else {
+						this.isShowClearHistory = true
+						this.show = true
+					}
+					console.log(this.history_list)
+				})
+			},
 			// 搜索
 			search(value) {
 				console.log("查找")
+				this.show = false
 				if (value != "") {
 					searchByKey({
 						key: value,
@@ -129,10 +224,26 @@
 </script>
 
 <style scoped>
+	.clear {
+		text-align: center;
+		width: 90%;
+		position: absolute;
+		bottom: 250rpx;
+	}
+
+	.search-history {
+		/* position: absolute; */
+		margin-top: 40%;
+		height: 200rpx;
+		min-width: 400rpx;
+	}
+
 	.search {
 		width: 100%;
 		display: flex;
-		margin-top: 90rpx;
+		margin-top: 88rpx;
+		z-index: 90077;
+		background: #ffffff;
 	}
 
 	.nav {
@@ -142,6 +253,8 @@
 		right: 0;
 		z-index: 1000;
 		background-color: white;
+		z-index: 90077;
+		background: #ffffff;
 	}
 
 	// 文本省略
